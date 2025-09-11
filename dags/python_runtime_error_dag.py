e173580da8ce1e6848a0405a05860a820506e9b1
akhilkurapati12 <125950481+akhilkurapati12@users.noreply.github.com>
2025-09-11 10:16:05 +0530

    fix: handle ZeroDivisionError

--- dags/python_runtime_error_dag.py
+++ dags/python_runtime_error_dag.py
@@ -9,8 +9,12 @@ def cause_a_division_by_zero_error():
     This function will always fail with a ZeroDivisionError.
     """
     logging.info("This task is about to fail...")
-    result = 1 / 0
-    logging.info(f"This line will never be reached. Result was {result}")
+    try:
+        result = 1 / 0
+    except ZeroDivisionError:
+        logging.error("Caught a ZeroDivisionError!")
+        result = None # or some other default value
+    logging.info(f"This line will now be reached. Result was {result}")
 
 with DAG(
     dag_id="python_runtime_error_dag",
