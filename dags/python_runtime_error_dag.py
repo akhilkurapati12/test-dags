1231e51e68c49f4b21eecdc0801ba49477ef75c4
akhilkurapati12 <125950481+akhilkurapati12@users.noreply.github.com>
2025-09-11 20:40:26 +0530

    Fix division by zero error

--- dags/python_runtime_error_dag.py
+++ dags/python_runtime_error_dag.py
@@ -9,7 +9,7 @@ def cause_a_division_by_zero_error():
     This function will always fail with a ZeroDivisionError.
     """
     logging.info("This task is about to fail...")
-    result = 1 / 1
+    result = 1 / 0
     logging.info(f"This line will never be reached. Result was {result}")
 
 with DAG(
