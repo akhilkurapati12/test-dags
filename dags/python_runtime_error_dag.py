76c5aebb7f1acb6f5d092c5afa7be1b3606747ee
akhilkurapati12 <125950481+akhilkurapati12@users.noreply.github.com>
2025-09-15 03:56:27 +0530

    Fix: Changed division by zero to division by one

--- dags/python_runtime_error_dag.py
+++ dags/python_runtime_error_dag.py
@@ -6,11 +6,11 @@ from airflow.operators.python import PythonOperator
 
 def cause_a_division_by_zero_error():
     """
-    This function will always fail with a ZeroDivisionError.
+    This function will now divide by 1 instead of 0 to avoid ZeroDivisionError.
     """
-    logging.info("This task is about to fail...")
-    result = 1 / 0
-    logging.info(f"This line will never be reached. Result was {result}")
+    logging.info("Performing division by 1...")
+    result = 1 / 1 # Changed from 1 / 0
+    logging.info(f"Result was {result}")
 
 with DAG(
     dag_id="python_runtime_error_dag",
