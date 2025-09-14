b7fddf13b2ce5272edbec903229d2ef8e2432d77
akhilkurapati12 <125950481+akhilkurapati12@users.noreply.github.com>
2025-09-15 00:18:49 +0530

    Fix: Cast XCom pulled value to int to prevent TypeError

--- dags/runtime_xcom_type_error_dag.py
+++ dags/runtime_xcom_type_error_dag.py
@@ -10,13 +10,15 @@ def push_a_string_value(**context):
     context["ti"].xcom_push(key="my_value", value="500")
 
 def pull_and_do_math(**context):
-    """Pulls the XCom value and tries to perform math with it."""
+    """
+    Pulls the XCom value and tries to perform math with it.
+    """
     pulled_value = context["ti"].xcom_pull(key="my_value", task_ids="push_task")
     logging.info(f"Pulled value '{pulled_value}' of type {type(pulled_value)} from XComs.")
     
     # THE ERROR IS HERE: You cannot add a string and an integer.
     # This will raise a TypeError.
-    result = pulled_value + 100
+    result = int(pulled_value) + 100 # Cast to int
     logging.info(f"This will not be logged. The result was {result}")
 
 with DAG(
