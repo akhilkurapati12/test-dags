d2cd4dd1078903d4109e824890b0f1c06110e628
akhilkurapati12 <125950481+akhilkurapati12@users.noreply.github.com>
2025-09-15 12:01:22 +0530

    Fix: Cast XCom value to int in runtime_xcom_type_error_dag.py

--- dags/runtime_xcom_type_error_dag.py
+++ dags/runtime_xcom_type_error_dag.py
@@ -16,7 +16,7 @@ def pull_and_do_math(**context):
     
     # THE ERROR IS HERE: You cannot add a string and an integer.
     # This will raise a TypeError.
-    result = pulled_value + 100
+    result = int(pulled_value) + 100  # Cast to int before adding
     logging.info(f"This will not be logged. The result was {result}")
 
 with DAG(
